de2821ac2e319ead6d14a0ea7dd9c197c3b33f0b
Yunus Emre Koca <ykoca.emre@gmail.com>
2023-05-10 22:08:29 +0300

    My_Helix_Jump
    All voices added

---
+++ Jump/Assets/Scenes/MenuScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void play()
+    {
+
+    }
+
+    public void quit()
+    {
+
+    }
+}
--- Jump/Assets/Scripts/Ball.cs
+++ Jump/Assets/Scripts/Ball.cs
@@ -9,9 +9,16 @@ public class Ball : MonoBehaviour
     private GameManager gm;
 
     public GameObject splashPrefab;
+    public GameObject backVoice;
+    public voiceController vc;  // hit sesi için
+    public FinishSounder fs; // bitiş sesi
+    public FailedSounder fail;
     void Start()
     {
         gm = GameObject.FindObjectOfType<GameManager>();
+        vc = GameObject.FindObjectOfType<voiceController>();
+        fs = GameObject.FindObjectOfType<FinishSounder>();
+        fail = GameObject.FindObjectOfType<FailedSounder>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,6 +30,7 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        vc.soundPlay();
         rb.velocity = Vector3.up*jumpForce; 
         GameObject splash = Instantiate(splashPrefab, transform.position + new Vector3(0, -0.25f, 0) , transform.rotation); // prefab tam temas etmiyordu, alçalttık. Önemli!
         splash.transform.SetParent(other.gameObject.transform);
@@ -31,13 +39,28 @@ public class Ball : MonoBehaviour
 
         if(name == "unsafeMaterial (Instance)")
         {
+<<<<<<< Updated upstream
             gm.restartGame();
+=======
+            Time.timeScale = 0;
+            fail.GetComponent<AudioSource>().Play();
+            backVoice.GetComponent<AudioSource>().Stop();
+            restartButton.SetActive(true);
+
+>>>>>>> Stashed changes
         }
 
         else if(name == "Bitis (Instance)")
         {
+<<<<<<< Updated upstream
             Debug.Log("Ball çalışıyor");
             gm.nextLevel();
+=======
+            Time.timeScale = 0;
+            fs.soundPlay();
+            nextButton.SetActive(true);
+            //gm.winText.gameObject.SetActive(true);
+>>>>>>> Stashed changes
         }
 
     }
